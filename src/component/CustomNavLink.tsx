6b1746cde8015b0c5f13627a0deb15945d158e33
Nelson Munthe <nnelson.munthe@gmail.com>
2024-11-26 13:48:15 +0700

    update home page & footer

--- src/component/CustomNavLink.tsx
+++ src/component/CustomNavLink.tsx
@@ -6,9 +6,8 @@ const CustomNavLink = ({description, path}:CustomNavLinkProps) => {
     return(
         <NavLink 
             to={path}
-            // className="rounded-md px-3 py-2 text-sm font-medium text-[#181A18] hover:bg-gray-200"
             aria-current="page"
-            className={({ isActive, isPending }) =>
+            className={({ isActive }) =>
                 isActive ? `${style} bg-gray-200` : `${style}`
             }
         >
